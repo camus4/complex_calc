c938a797434bf15abd2506ecbc190b8bd0331de9
camuu <123274257+camus4@users.noreply.github.com>
2024-05-31 01:20:26 +0200

    main.py

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import cmath
 import math
 print("better version of calculator, choose a module")
 
-choose_mod = input("1: komplexe zahlen \n2: logarithmen lösen \n3: erklärung für das jeweilige modul "  )
+choose_mod = input("1: complex numbers \n2: logarithms \n3: explain the module "  )
 
 def module_1():
     while True:
         try:
-            print("komplexe zahlen")
-            choose_operator = input("+ - * / oder x zum beenden: ")
+            print("complex numbers")
+            choose_operator = input("+ - * / or x to end: ")
             if choose_operator.lower() == "x":
                 return
 
-            c_num1 = complex(input("erset zahl (a + bj)"))
-            c_num2 = complex(input("zweite: "))
+            c_num1 = complex(input("first number:  (a + bj)"))
+            c_num2 = complex(input("second:  "))
 
 
             if choose_operator.lower() == "+":
@@ -35,13 +35,13 @@ def module_1():
 def module_2():
     while True:
         try:
-            print("logarithmen")
-            base = float(input("basis: "))
-            zahl = float(input("zahl: "))
+            print("logarithms")
+            base = float(input("base: "))
+            number = float(input("number: "))
 
-            result = math.log(zahl, base)
+            result = math.log(number, base)
             print(result)
-            cancel_mod_2 = input("mit enter weitermachen oder 'x' zu, beenden drücken: ")
+            cancel_mod_2 = input("continue with enter or x to kill: ")
             if cancel_mod_2.lower() == "x":
                 return
 
@@ -49,18 +49,17 @@ def module_2():
             print()
 
 def module_3():
-    print("erklärung für das jeweilige modul")
+    print("explanation")
     while True:
-        exp_1 = "komplexe zahlen: Diese sind da, um z.B die Gleichung x ^2 + 1 = 0 zu lösen. hier wäre x = die wurzel aus -1, das ist ein mal i (imaginäre einheit) die wurzel aus -2 wäre auf der imaginären achse 1,41... i. jede zahl, also alle natürlichen zahlen etc sind gleichzeitig auch komplexe zahlen. Eine komplexe zahl c setzt sich so zusammen : c = a + b * i. a gleich realteil und b der imaginärteil. beispiel: wurzel -25 + 4 = 5i + 4"
-        exp_2 = "logarithmen: logarithmieren ist das gegenteil zum potenzieren. z.B 5^ 2 = 25. log5 (25) = 2. also stellt sich die frage 'was hoch die basis (5) ergibt die zahl (25)? ' "
-        which_exp = input("nummer des moduls das du erklärt haben möchtest oder 'x' zum beenden: ")
+        exp_1 = "complex numbers: These exist to solve equations like x^2 + 1 = 0. Here, x would be the square root of -1, which is i (imaginary unit). The square root of -2 would be 1.41... i on the imaginary axis. Every number, including all natural numbers, are also complex numbers. A complex number c is composed as follows: c = a + b * i. a is the real part and b is the imaginary part. Example: square root of -25 + 4 = 5i + 4"
+        exp_2 = "logarithms: logarithms are the opposite of exponentiation. For example, 5^2 = 25. log5 (25) = 2. So the question is 'what power of the base (5) gives the number (25)?'"
+        which_exp = input("number of the module you want explained or 'x' to exit: ")
         if which_exp.lower() == "1":
             print(exp_1)
         if which_exp.lower() == "2":
             print(exp_2)
         if which_exp.lower() == "x":
             return
-
 
 if choose_mod == "1":
     module_1()
@@ -69,7 +68,7 @@ if choose_mod == "2":
 if choose_mod == "3":
     module_3()
 while True:
-    end_any = input("nummer deines moduls oder 'x' zum beenden: ")
+    end_any = input("number of your module or 'x' to kill: ")
 
     if end_any.lower() == "1":
         module_1()
